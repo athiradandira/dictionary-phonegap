249f13aa95adce7ab0e7187da31949974b0f078b
tvhoang75 <tvhoang75@gmail.com>
2012-12-01 14:06:25 +0000

    started the project

---
+++ src/com/cntt/dictionary.java
@@ -0,0 +1,15 @@
+package com.cntt;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class dictionary extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
